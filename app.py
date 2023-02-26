884d3fa7873079d694003b6f2c84322a2c2babea
Ravikiran0011 <125904929+Ravikiran0011@users.noreply.github.com>
2023-02-26 22:35:31 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+
+st.title("My :red[Curriculum] Vitae")
+st.title("")
+st.markdown("Bareddy Ravikiranreddy (Data Science Intern)")
+st.markdown("bogolu, cumbum,andhrapradesh")
+st.markdown("**Ph**- 8106335283")
+st.markdown("**Email**- ds.ravikiranreddy0508@gmail.com")
+st.markdown("**linkedin**- https://www.linkedin.com/in/ravikiranreddy0507/")
+st.markdown("**Objective:** To obtain a challenging position as a Data Scientist where I can utilize my skills and knowledge to contribute to the growth and success of the company.")
+
+st.markdown("**Work Experience:** Internship at Innomatics Research Labs(feb 2023): working on data cleaning and analysis tasks on various projects. Contributed to the development of a predictive model for customer retention using different types of algorithms.")
+
+st.markdown("**Programming languages:** Python, SQL")
+st.markdown("**Data analysis tools:** Excel, Tableau")
+st.markdown("**Machine learning algorithms:** Regression, Decision Trees, Random Forest, SVM, K-Means Clustering")
+
